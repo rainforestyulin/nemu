3dad7245bb90f27f75a65f2aa13d51398b27911d
tracer-ics2020 <tracer@njuics.org>
2022-04-11 05:22:41 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 2efa05cfc8e949c9a57d8dbade078382            Boot ID: 48f098ddf2c54157b052ea0777998ae9     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0       Architecture: x86-64  05:22:41 up  9:23,  1 user,  load average: 0.15, 0.09, 0.02

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,7 +38,9 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
-
+static int cmd_s(char *args){
+  return -2;
+}
 static struct {
   char *name;
   char *description;
@@ -47,6 +49,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  {"si [N]", "Signal step execute", cmd_s },
 
   /* TODO: Add more commands */
 
