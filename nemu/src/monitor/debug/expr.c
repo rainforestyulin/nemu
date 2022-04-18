aeb40942de9b160f449d047a3b65ede7b600e2eb
tracer-ics2020 <tracer@njuics.org>
2022-04-18 01:33:26 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 2efa05cfc8e949c9a57d8dbade078382            Boot ID: 48f098ddf2c54157b052ea0777998ae9     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0       Architecture: x86-64  01:33:26 up 1 day,  7:42,  1 user,  load average: 0.01, 0.04, 0.00

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -86,10 +86,14 @@ static bool make_token(char *e) {
 
         switch (rules[i].token_type) {
 		case TK_NUM:
-			tokens[nr_token++].type=rules[i].token_type;
+			tokens[nr_token].type=rules[i].token_type;
 	      		for(int j=0;j<substr_len-1;j++){
-	          	tokens[nr_token++].str[j]=*(substr_start+j);
-	       };
+	          		tokens[nr_token].str[j]=*(substr_start+j);
+	       		};
+			nr_token++;
+		case TK_RPAR:
+			tokens[nr_token].type=rules[i].token_type;
+			tokens[nr_token].str[0]=*(substr_start);
           default: TODO();
         }
 
