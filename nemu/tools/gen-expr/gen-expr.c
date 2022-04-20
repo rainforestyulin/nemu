abdd31b6f9ad87fa88d478d4c1fa9523dad7393b
tracer-ics2020 <tracer@njuics.org>
2022-04-20 00:36:24 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 2efa05cfc8e949c9a57d8dbade078382            Boot ID: 48f098ddf2c54157b052ea0777998ae9     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0       Architecture: x86-64  00:36:24 up 1 day, 18:36,  1 user,  load average: 0.03, 0.05, 0.02

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -6,6 +6,7 @@
 #include <string.h>
 
 // this should be enough
+static int buf_nr=0;
 static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
@@ -15,9 +16,65 @@ static char *code_format =
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
+void   val_deal(int val,char *c){
+        int depth=0;
+	int tempp=10;
+	int temp=val;
+	printf("val_is_%d\n",val);
+	while(temp>10){
+		temp=temp/10;
+		depth++;
+	}
+	*(c+buf_nr)=temp+48;
+	buf_nr++;
+	printf("add_%c\n",temp+48);
+	for(int i=0;i<depth-1;i++){
+		tempp=tempp*tempp;
+	}
+	if((val-temp*tempp)>0){
+		val_deal(val-temp*tempp,c);
+	}
+};
+int choose(int val){
+      int p=rand()%(val-1);
+      printf("choose is %d\n",p);
+      return p;
+};
+void gen_num(){
+        int temp=0;
+        srand((int)time(0));
+        temp=rand()%1000;
+	printf("temp_is_%d \n",temp);
+        val_deal(temp,buf);
+	/*for(int i=0;i<buf_nr;i++){
+	printf("%c\n",buf[i]);
+	}*/
+};
+void gen(int a){
+        buf[buf_nr++]=a;
+	printf("add_%c\n",a);
+};
+void gen_rand_op(){
+        switch(choose(4)){
+                case 0: buf[buf_nr++]='+';
+			printf("add_op%c\n",'+');
+                case 1: buf[buf_nr++]='-';
+			printf("add_op%c\n",'-');
+                case 2: buf[buf_nr++]='*';
+			printf("add__op%c\n",'*');
+                case 3: buf[buf_nr++]='/';
+			printf("add_op_%c\n",'/');
+        }
+};
 
-static inline void gen_rand_expr() {
-  buf[0] = '\0';
+
+static void gen_rand_expr() {
+	switch (choose(3)) {
+  	case 0: gen_num(); break;
+    	case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    	default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  };
+	//  buf[0] = '\0';
 }
 
 int main(int argc, char *argv[]) {
@@ -45,7 +102,7 @@ int main(int argc, char *argv[]) {
     assert(fp != NULL);
 
     int result;
-    fscanf(fp, "%d", &result);
+    assert(fscanf(fp, "%d", &result));
     pclose(fp);
 
     printf("%u %s\n", result, buf);
