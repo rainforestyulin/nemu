c169b520b8c9ebaeab597f0855746ecd94aae891
tracer-ics2020 <tracer@njuics.org>
2022-04-23 02:00:56 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 2efa05cfc8e949c9a57d8dbade078382            Boot ID: 48f098ddf2c54157b052ea0777998ae9     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0       Architecture: x86-64  02:00:56 up 2 days,  7:06,  1 user,  load average: 0.08, 0.04, 0.01

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -75,6 +75,7 @@ void isa_reg_display() {
 word_t isa_reg_str2val(const char *s, bool *success) {
 	int i;
   for (i = R_EAX; i <= R_EDI; i ++) {
+	  printf("%s -- %s\n",s,regsl[i]);
     if(strcmp(s,regsl[i])){
       *success=true;
       break;
