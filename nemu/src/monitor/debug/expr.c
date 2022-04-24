a03aa6cb09cf4ee5a36e03c577feae9048b2e8eb
tracer-ics2020 <tracer@njuics.org>
2022-04-24 00:07:17 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 2efa05cfc8e949c9a57d8dbade078382            Boot ID: 956bf393feae466cb2571121636ff5c1     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0       Architecture: x86-64  00:07:17 up 43 min,  1 user,  load average: 0.01, 0.02, 0.04

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -283,7 +283,7 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-  eval(0,nr_token-1);
+  eval(0,nr_token);
   
   /* TODO: Insert codes to evaluate the expression. */
 //  TODO();
