1e2037ca011762a0e69eb92ed34ec79072e4999c
tracer-ics2020 <tracer@njuics.org>
2022-04-27 20:25:25 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 2efa05cfc8e949c9a57d8dbade078382            Boot ID: 956bf393feae466cb2571121636ff5c1     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0       Architecture: x86-64  20:25:25 up 19:42,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -379,7 +379,7 @@ if (p > q) {
 				i=k;
 				Log("ptr_end_%d",i);
 				Log("q is%d",q);
-				if(i<q&&tokens[i+1].type!=TK_R&&tokens[i+1].type!=TK_NUM&&tokens[i+1].type!=TK_LPAR&&tokens[i+1].type!=TK_RPAR&&tokens[i+1].type!=TK_PTR) {
+				if(i<q&&tokens[i+1].type!=TK_R&&tokens[i+1].type!=TK_NUM&&tokens[i+1].type!=TK_LPAR&&tokens[i+1].type!=TK_RPAR) {
 					Log("detetc");
 					continue;
 				}else {
