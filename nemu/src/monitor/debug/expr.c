a19725e70a0fdd0bf0e89ffccd163f355e6bb9c4
tracer-ics2020 <tracer@njuics.org>
2022-04-27 20:24:18 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 2efa05cfc8e949c9a57d8dbade078382            Boot ID: 956bf393feae466cb2571121636ff5c1     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0       Architecture: x86-64  20:24:18 up 19:41,  1 user,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -378,6 +378,7 @@ if (p > q) {
 				val1= vaddr_read1(addr);
 				i=k;
 				Log("ptr_end_%d",i);
+				Log("q is%d",q);
 				if(i<q&&tokens[i+1].type!=TK_R&&tokens[i+1].type!=TK_NUM&&tokens[i+1].type!=TK_LPAR&&tokens[i+1].type!=TK_RPAR&&tokens[i+1].type!=TK_PTR) {
 					Log("detetc");
 					continue;
