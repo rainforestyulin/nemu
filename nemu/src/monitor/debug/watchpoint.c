2d81b588ab95acff48ff7fb08a10f8c628d553c5
tracer-ics2020 <tracer@njuics.org>
2022-05-09 05:41:54 -0700

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 2efa05cfc8e949c9a57d8dbade078382            Boot ID: d4128c0a2d9b435eb2c6e53401aa1575     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0       Architecture: x86-64  05:41:54 up  7:16,  1 user,  load average: 0.02, 0.02, 0.00

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -21,3 +21,60 @@ void init_wp_pool() {
 /* TODO: Implement the functionality of watchpoint */
 WP* new_wp();
 void free_wp(WP *wp);
+WP* new_wp(){
+	/*if(*head==NULL){
+		head=free_;
+		free_=free_->next;
+		head->next=NULL;
+		return head;
+	}else{
+		if(free_!=NULL){
+			WP* temp=head;
+			while(head!=NULL){
+				head=head->next;
+			}
+			head=free_;
+			head->next=NULL;
+			free_=free_->next;
+		}else{
+			assert(-1);
+		}
+	}*/
+	if(free_!=NULL){
+		WP* temp=head;
+		while(temp!=NULL){
+			temp=temp->next;
+		};
+		temp=free_;
+		free_=free_->next;
+		return temp;
+
+	}else{
+		assert(-1);
+		return 0;
+	}
+}
+void free_wp(WP *wp){
+	WP* temp=head;
+	unsigned int index=0;
+	while(temp->NO!=wp->NO&&index<=NR_WP){
+		temp=temp->next;
+		index++;
+	}
+	assert(index<=NR_WP);
+	if(temp==head){
+		WP* ttemp=free_;
+		free_=temp;
+		free_->next=ttemp;
+		head=NULL;
+	}else{
+		temp=head;
+		while(index-1>0){
+			temp=temp->next;
+		}
+		temp->next=temp->next->next;
+		WP* ttemp=free_;
+		free_=temp;
+		free_->next=ttemp;
+	}
+}
